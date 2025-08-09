171b87ca636c4fe1faeb3b5f4843b5bdd0a0bb50
Pedro Bezerra <111087418+Pdrobzr@users.noreply.github.com>
2025-08-09 15:07:50 -0300

    primeiro commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        return UserResource::collection($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $data['password'] = bcrypt($request->password);
+
+        $user = User::create($data);
+
+        return response()->json(new UserResource($user), 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = User::findOrFail($id);
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = User::findOrFail($id);
+
+        $data = $request->all();
+
+        $data['password'] = bcrypt($request->password);
+
+        $user->update($data);
+
+        return response()->json(new UserResource($user), 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::findOrFail($id);
+
+        User::destroy($user['id']);
+
+        return response()->json("Usuário deletado com sucesso!", 200);
+    }
+}
